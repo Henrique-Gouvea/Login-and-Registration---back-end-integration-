923de5f7972828cd5d4cbf16206d8ba2f9c881d0
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2022-05-21 10:14:29 -0300

    feat: context in login ok

--- front/src/pages/login/index.jsx
+++ front/src/pages/login/index.jsx
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import CadasterContext from "../../context/CadasterContext";
 import ButtonApi from "../components/buttonapi";
 
 function Login() {
+    const {
+        nameLogin,
+        setNameLogin,
+        passwordLogin,
+        setPasswordLogin,
+    } = useContext(CadasterContext);
+
     return(
       <>
-        <h1>Login</h1>
-        <form>
-          <label htmlFor="name-login">
-            Name
-            <input
-              name="name-login"
-              type="email"
-            //   onChange={ (ele) => setNameLogin(
-            //     ele.target.value,
-            //   ) }
-            //   value={ nameLogin }
-            />
-          </label>
-          <label htmlFor="password-login">
-            Password
-            <input
-              type="password"
-              name="password-login"
-            //   onChange={ (ele) => setPasswordLogin(
-            //     ele.target.value,
-            //   ) }
-            //   value={ passwordLogin }
-            />
-          </label>
-          <ButtonApi />
-        </form>
+        <div>
+					<h1>Login</h1>
+					<div>
+						<form>
+							<label htmlFor="name-login">
+								Name
+								<input
+									name="name-login"
+									type="email"
+									onChange={ (ele) => setNameLogin(
+										ele.target.value,
+									) }
+									value={ nameLogin }
+								/>
+							</label>
+							<label htmlFor="password-login">
+								Password
+								<input
+									type="password"
+									name="password-login"
+									onChange={ (ele) => setPasswordLogin(
+										ele.target.value,
+									) }
+									value={ passwordLogin }
+								/>
+							</label>
+							<ButtonApi />
+						</form>
+					</div>
+        </div>
       </>
     )
   }
